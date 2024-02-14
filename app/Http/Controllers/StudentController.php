4c0040c5a03c697d13521118541dda94fe89ec31
Ssnnee <nandisamuelsne@gmail.com>
2024-02-14 08:30:57 +0100

    feat: delete and read

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $students = Student::all();
+        return view('students.index', compact('students'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('students.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+        Student::create($request->all());
+        Student::create($request->all());
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required|numeric',
+            'address' => 'required',
+        ]);
+
+        Student::create($request->all());
+
+
+        return redirect('/students')->with('success', 'Student has been added');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,53 @@
+@extends('base')
+
+@section('main')
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="display-3">Enseignants</h1>
+            <div>
+                <a style="margin: 19px;" href="{{ route('students.create')}}" class="btn btn-primary">Ajouter un  étudiant</a>
+            </div>
+
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td></td>
+                        <td>Prenom</td>
+                        <td>Nom</td>
+                        <td>Email</td>
+                        <td>Telephone</td>
+                        <td>Adresse</td>
+
+                        <td colspan="2">Actions</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($students as $student)
+                        <tr>
+                            <td>{{$student->id}}</td>
+                            <td>{{$student->first_name}}</td>
+                            <td>{{$student->last_name}}</td>
+                            <td>{{$student->email}}</td>
+                            <td>{{$student->phone}}</td>
+                            <td>{{$student->address}}</td>
+
+                            <td>
+                                <a href="{{ route('students.edit', $student->id)}}" class="btn btn-primary">Edit</a>
+                            </td>
+                            <td>
+                                <form action="{{ route('students.destroy', $student->id)}}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger" type="submit">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="col-sm-12"> @if(session()->get('success'))
+    <div class="alert alert-success"> {{ session()->get('success') }} </div> @endif</div>
+    </div>
+@endsection
